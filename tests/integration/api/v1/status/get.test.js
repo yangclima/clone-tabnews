0d2a50356280aaebf2414439833960c47c497fe4
yangclima <yangclima@gmail.com>
2025-04-21 23:16:56 -0300

    finish /api/v1/status/ endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,6 +1,13 @@
-describe("GET /api/v1/status/", () => {
-  test("Should return 200", async () => {
-    const response = await fetch("http://localhost:3000/api/v1/status");
-    expect(response.status).toBe(200);
-  });
+test("GET to /api/v1/status", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  expect(response.status).toBe(200);
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(parseUpdatedAt).toEqual(responseBody.updated_at);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
